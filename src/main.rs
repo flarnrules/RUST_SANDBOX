b1d1dbac2d6396ad66c8dda38e9444fd62119dc5
flarnrules <breifenberg11@gmail.com>
2022-11-03 10:47:55 -0500

    added cli.rs, conditionals.rs, enums.rs, functions.rs, loops.rs, pointer_ref.rs, structs.rs, vectors.rs and changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@
 // mod vars;
 // mod types;
 // mod strings;
-mod tuples;
+// mod tuples;
+// mod arrays;
+// mod vectors;
+// mod conditionals;
+// mod loops;
+// mod functions;
+// mod pointer_ref;
+// mod structs;
+// mod enums;
+mod cli;
 
 
 fn main() {
@@ -11,5 +20,14 @@ fn main() {
     // types::run();
     // strings::run();
     // strings::run();
-    tuples::run();
+    // tuples::run();
+    // arrays::run();
+    // vectors::run();
+    // conditionals::run();
+    // loops::run();
+    // functions::run();
+    // pointer_ref::run();
+    // structs::run();
+    // enums::run();
+    cli::run();
 }
